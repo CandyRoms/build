cfff11434dc643c28ed1444032bc8ff0d41874e4
Tom Marshall <tdm.code@gmail.com>
2017-11-09 20:59:44 +0100

    build: Switch kernel header inclusion
    The build is switching to the target INSTALLED_KERNEL_HEADERS to
    declare dependencies on kernel headers.
    
    Change-Id: I913e74681b02dfcf1eaed3d1e47ff4ab2300b12d

--- core/binary.mk
+++ core/binary.mk
@@ -51,7 +51,16 @@ endif
 # just include generated_kernel_headers which already has the proper
 # dependency
 ifneq (,$(findstring $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,$(LOCAL_ADDITIONAL_DEPENDENCIES)))
-  LOCAL_ADDITIONAL_DEPENDENCIES := $(patsubst $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,,$(LOCAL_ADDITIONAL_DEPENDENCIES))
+  LOCAL_ADDITIONAL_DEPENDENCIES := $(patsubst $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,INSTALLED_KERNEL_HEADERS,$(LOCAL_ADDITIONAL_DEPENDENCIES))
+endif
+
+# Many qcom modules don't correctly set a dependency on the kernel headers. Fix it for them,
+# but warn the user.
+ifneq (,$(findstring $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include,$(LOCAL_C_INCLUDES)))
+  ifeq (,$(findstring INSTALLED_KERNEL_HEADERS,$(LOCAL_ADDITIONAL_DEPENDENCIES)))
+    $(warning $(LOCAL_MODULE) uses kernel headers, but does not depend on them!)
+    LOCAL_ADDITIONAL_DEPENDENCIES += INSTALLED_KERNEL_HEADERS
+  endif
 endif
 
 # The following LOCAL_ variables will be modified in this file.
