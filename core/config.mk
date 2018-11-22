737c9fd7b5e038adc8c9ad4cc74f4a2c3667477f
Julian Veit <claymore1298@gmail.com>
2018-11-22 21:30:41 +0100

    build: include candy-sepolicy common directories
    Change-Id: I005d6ef83cc51afb61329c1d0d30761474b786ba

--- core/config.mk
+++ core/config.mk
@@ -1155,7 +1155,7 @@ endif
 ifneq ($(CANDY_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include device/candy/sepolicy/common/sepolicy.mk)
+$(eval include device/candy/sepolicy/common/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
