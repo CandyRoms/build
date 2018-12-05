259ff4939f098747820e3111a3053d56c40d2ea1
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:49 +0100

    build: Support system-as-root A-only backuptool
    Change-Id: Ia24ae288a48721f724bfebadfe023330c4932633
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -965,9 +965,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
   if OPTIONS.backuptool:
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/system"
     script.Mount("/system")
-    script.RunBackup("backup")
+    script.RunBackup("backup", "/system/system" if is_system_as_root else "/system")
     script.Unmount("/system")
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/"
 
   system_progress = 0.75
 
@@ -1028,21 +1033,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
     if is_system_as_root:
-      script.fstab["/system"].mount_point = system_mount_point
+      script.fstab["/system"].mount_point = "/system"
     script.Mount("/system")
-    if is_system_as_root and common.system_as_system:
-      script.RunBackup("restore", "/system/system")
-    else:
-      script.RunBackup("restore", "/system")
-    script.Unmount(system_mount_point)
+    script.RunBackup("restore", "/system/system" if is_system_as_root else "/system")
+    script.Unmount("/system")
     if is_system_as_root:
       script.fstab["/system"].mount_point = "/"
-
-  device_specific.FullOTA_PostValidate()
-    script.Mount("/system")
-    script.RunBackup("restore")
-    script.Unmount("/system")
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
