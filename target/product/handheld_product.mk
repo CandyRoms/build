df4518136c28ca55b36cb8f0fa7138b2872256e6
Kshitij Gupta <kshitijgm@gmail.com>
2019-03-14 12:51:28 +0000

    product: Remove unused packages
    Change-Id: I225a20bc4c5c6598247c366256d4ae3d980a2360
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,7 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Browser2 \
     Calendar \
     Camera2 \
     Contacts \
@@ -30,10 +29,8 @@ PRODUCT_PACKAGES += \
     Email \
     Gallery2 \
     Launcher3QuickStep \
-    Music \
     OneTimeInitializer \
     Provision \
-    QuickSearchBox \
     Settings \
     SettingsIntelligence \
     StorageManager \
