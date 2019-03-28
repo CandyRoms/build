f6a014fa5cf1011ad0248bc88f683628ce79d70c
Anton Hansson <hansson@google.com>
2019-03-28 15:45:40 +0000

    Access PRODUCT_ variables directly
    This CL simplifies the PRODUCTS.$(INTERNAL_PRODUCT).X accesses of
    product variables, and removes unnecessary stripping of them.
    
    Replace: '\$\(PRODUCTS\.\$\(INTERNAL_PRODUCT\)\.([^\)]*)\)' with '$(\1)'
    Replace: '\$\(strip\s*\$\(PRODUCT_([^\)]*)\)\)' with '$(PRODUCT_\1)'
    
    A few minor manual tweaks.
    
    Bug: 116769560
    Test: presubmit
    Change-Id: I70c54f1582e3cc780028535960147d99ebc2e0e1

--- core/tasks/vendor_module_check.mk
+++ core/tasks/vendor_module_check.mk
@@ -48,7 +48,7 @@ _vendor_owner_whitelist := \
         widevine
 
 
-_restrictions := none
+_restrictions := $(PRODUCT_RESTRICT_VENDOR_FILES)
 
 ifneq (,$(_restrictions))
 ifneq (,$(VENDOR_PRODUCT_RESTRICT_VENDOR_FILES))
