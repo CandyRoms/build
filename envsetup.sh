81286c1ef658a0c76ee59c80a2cf154ccec751e7
Dan Cartier <nospamdan1@gmail.com>
2019-09-21 08:52:25 -0400

    Add in roomservice
    This is a new implementation of roomservice
    use the new github search api to reduce the memory footprint, and make it less processor intensive
    allow for unofficial devices to fetch dependencies. just specify your github organization
    
    to fetch dependencies:
        add a file called 'omni.dependencies' to the root of your device tree
        an example configuration for the dependency file is:
    
    [
      {
        "repository": "android_device_sony_common",
        "target_path": "device/sony/common"
      },
      {
        "repository": "legacybop/android_hardware_qcom_display-legacy",
        "target_path": "hardware/qcom/display-legacy",
        "revision": "omni-4.3"
      }
    
    please note that comments can not go into this file. It must be valid json
    
    PatchSet 7:
     add support to change port script to another team with config
     finished pep8'ing
     more checks on manifest parsing
    
    Change-Id: Icf6e107a1f52b27f5148090ccbc1e6df1077f77c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -674,18 +674,15 @@ function lunch()
     if [ $? -ne 0 ]
     then
         # if we can't find the product, try to grab it from our github
-        T=$(gettop)
-        C=$(pwd)
-        cd $T
+        pushd $T > /dev/null
         $T/vendor/candy/build/tools/roomservice.py $product
-        cd $C
+        popd > /dev/null
         check_product $product
     else
         T=$(gettop)
-        C=$(pwd)
-        cd $T
+        pushd $T > /dev/null
         $T/vendor/candy/build/tools/roomservice.py $product true
-        cd $C
+        popd > /dev/null
     fi
     if [ $? -ne 0 ]
     then
