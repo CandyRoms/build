89d6bb3922428b66c8caf0fa976ea8adc25c5ff7
NurKeinNeid <mralexman3000@gmail.com>
2022-07-12 17:01:59 +0200

    releasetools: Correct flashing and verified text
    Change-Id: I15b0e034094c0cf4b60a86f5f26fc8d77ab50e54
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3416,10 +3416,10 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing CandyRoms %s files..." % (self.partition,))
+      script.Print("Flashing CandyRoms %s partition..." % (self.partition,))
     else:
       script.Print(" ")
-      script.Print("Flashing CandyRoms %s files after verification." % (self.partition,))
+      script.Print("Flashing CandyRoms %s partition after verification." % (self.partition,))
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -3538,7 +3538,7 @@ class BlockDifference(object):
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
       script.Print(" ")
-      script.Print('Verified CandyRoms %s files.' % (partition,))
+      script.Print('Verified CandyRoms %s partition.' % (partition,))
       if partition == "system":
         code = ErrorCode.SYSTEM_NONZERO_CONTENTS
       else:
